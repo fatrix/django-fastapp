1a0eea30b495e19647ee9c2b83d94ad566dd7ca9
Philip Sahli <ilipcito@gmail.com>
2013-12-04 07:14:32 +0100

    add versions, fix STATIC_URL

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='fastapp',
-      version='0.1',
+      version='0.2',
       py_modules=['fastapp'],
-      install_requires=['dropbox', 'requests'],
+      install_requires=['dropbox==1.6', 'requests==2.0.1', 'django_extensions==1.1.1'],
 )
