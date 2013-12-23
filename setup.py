76bf998a38e24f902a387df445ba26d9635b3cf7
Philip Sahli <ilipcito@gmail.com>
2013-12-23 08:33:05 +0100

    add pusher

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from distutils.core import setup
 
 setup(name='fastapp',
-      version='0.2',
+      version='0.3',
       py_modules=['fastapp'],
-      install_requires=['dropbox==1.6', 'requests==2.0.1', 'django_extensions==1.1.1'],
+      install_requires=['dropbox==1.6', 
+      					'requests==2.0.1', 
+      					'django_extensions==1.1.1', 
+						'pusher==0.8',
+						'mongoengine==0.8.6',
+						'pymongo==2.6.3]'
+						]
 )
