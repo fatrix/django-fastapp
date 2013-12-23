5f113e9135c337e6b252934db850377696ad5225
Philip Sahli <ilipcito@gmail.com>
2013-12-23 08:59:50 +0100

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='fastapp',
-      version='0.3',
+      version='0.31',
       py_modules=['fastapp'],
       install_requires=['dropbox==1.6', 
       					'requests==2.0.1', 
