2003d882a35bb2844ea4d237d0128905378a373e
Philip Sahli <philip@sahli.net>
2014-05-04 09:03:50 +0200

    fix pika import error

--- fastapp/__init__.py
+++ fastapp/__init__.py
@@ -1,10 +1,10 @@
-__version__ = "0.411"
+__version__ = "0.412"
 
 import pusher
-import pika
-import threading
+#import pika
+#import threading
 from django.conf import settings
-from fastapp.models import Base, Executor
+#from fastapp.models import Base, Executor
 
 #monkey.patch_all()
 def get_pusher_instance():
@@ -23,29 +23,28 @@ def callback(ch, method, properties, body):
 	channel = payload['channel']
 	event = payload['event']
 	data = payload['data']
-	print "CALLBACK", body
 	pusher_instance[channel].trigger(event, data)
 
 
-def send_events():
-	print "START - SEND_EVENTS Consumer"
-	connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-	channel = connection.channel()
-	channel.basic_consume(callback,
-                      queue='pusher_events',
-                      #no_ack=True
-	)	
-	print "END - SEND_EVENTS Consumer"
+#def send_events():
+#	print "START - SEND_EVENTS Consumer"
+#	connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+#	channel = connection.channel()
+#	channel.basic_consume(callback,
+#                      queue='pusher_events',
+#                      #no_ack=True
+#	)	
+#	print "END - SEND_EVENTS Consumer"
 	
-def start_listen_for_events():
-	print "Start events dispatcher thread"
-	threads = []
-	for c in range(1, 10):
-		#threads.append(gevent.spawn(send_events))
-		#thread.start_new_thread(send_events, ())
-		t = threading.Thread(target=send_events, args=())
-		t.start()
-		threads.append(t)
+#def start_listen_for_events():
+#	print "Start events dispatcher thread"
+#	threads = []
+#	for c in range(1, 10):
+#		#threads.append(gevent.spawn(send_events))
+#		#thread.start_new_thread(send_events, ())
+#		t = threading.Thread(target=send_events, args=())
+#		t.start()
+#		threads.append(t)
 
 	#for t in threads:
 	#	t.join()
